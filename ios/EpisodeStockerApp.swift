2ccd0c8415628dbbea5df49a79411c7138c0bb82
comado <comado.studio@gmail.com>
2026-02-17 20:56:09 +0900

    fix: disable SwiftData CloudKit sync in app and tests

--- ios/EpisodeStockerApp.swift
+++ ios/EpisodeStockerApp.swift
@@ -5,23 +5,36 @@ import SwiftUI
 struct EpisodeStockerApp: App {
     @StateObject private var store = EpisodeStore()
     @StateObject private var router = AppRouter()
+    private let modelContainer: ModelContainer
+
+    init() {
+        let isRunningTests = ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+        let configuration = ModelConfiguration(
+            isStoredInMemoryOnly: isRunningTests,
+            cloudKitDatabase: .none
+        )
+        do {
+            modelContainer = try ModelContainer(
+                for: Episode.self,
+                UnlockLog.self,
+                Tag.self,
+                Person.self,
+                Project.self,
+                Emotion.self,
+                Place.self,
+                configurations: configuration
+            )
+        } catch {
+            fatalError("Failed to create ModelContainer: \(error)")
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
             RootTabContainer()
                 .environmentObject(store)
                 .environmentObject(router)
-                .modelContainer(
-                    for: [
-                        Episode.self,
-                        UnlockLog.self,
-                        Tag.self,
-                        Person.self,
-                        Project.self,
-                        Emotion.self,
-                        Place.self
-                    ]
-                )
+                .modelContainer(modelContainer)
         }
     }
 }
